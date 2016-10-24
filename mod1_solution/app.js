e305e83d925200fb7ea6c3fd201d731a820c1dc9
Prashanth <janagama.prashanth@gmail.com>
2016-10-24 00:11:25 -0400

    Validation

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -11,6 +11,10 @@ function LunchCheckController($scope){
 	$scope.msg = "";
 	$scope.dishes = "";
 	$scope.checkIfTooMuch = function(){
+		if($scope.dishes===""){
+			$scope.msg = "Please enter data first.";
+			return;
+		};
     var listOfdishes =  $scope.dishes.split(',');
      if(listOfdishes.length<=3){
         $scope.msg = "Enjoy!";
